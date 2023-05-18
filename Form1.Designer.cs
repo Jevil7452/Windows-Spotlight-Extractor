ef6adcfb660a6f58f3fc1e733055ab1161017a1d
Jevil7452 <98893064+Jevil7452@users.noreply.github.com>
2023-05-18 23:09:52 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Spotlight_Extract
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(457, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "This tool will extract all Windows Spotlight images to a folder on your desktop n" +
+    "amed \"Spotlight\"";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Extract (Landscape)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(138, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Extract (Portrait)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(238, 30);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(69, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Extract (All)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 64);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spotlight_Extract
+{
+    public partial class Form1 : Form
+    {
+        string folder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Spotlight";
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(path))
+            {
+                if (!Directory.Exists(this.folder))
+                {
+                    Directory.CreateDirectory(this.folder);
+                }
+                foreach (string text in Directory.GetFiles(path))
+                {
+                    try
+                    {
+                        Image image = Image.FromFile(text);
+                        if (image.Width > image.Height)
+                        {
+                            string str2 = "Landscape_" + Path.GetFileName(text) + ".jpg";
+                            File.Copy(text, folder + "\\" + str2, true);
+                        }
+
+                        image.Dispose();
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(path))
+            {
+                if (!Directory.Exists(this.folder))
+                {
+                    Directory.CreateDirectory(this.folder);
+                }
+                foreach (string text in Directory.GetFiles(path))
+                {
+                    try
+                    {
+                        Image image = Image.FromFile(text);
+                        if (image.Width < image.Height)
+                        {
+                            string str2 = "Portrait_" + Path.GetFileName(text) + ".jpg";
+                            File.Copy(text, folder + "\\" + str2, true);
+                        }
+
+                        image.Dispose();
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(path))
+            {
+                if (!Directory.Exists(this.folder))
+                {
+                    Directory.CreateDirectory(this.folder);
+                }
+                foreach (string text in Directory.GetFiles(path))
+                {
+                    try
+                    {
+                        Image image = Image.FromFile(text);
+                        if (image.Width > image.Height)
+                        {
+                            string str2 = "Landscape_" + Path.GetFileName(text) + ".jpg";
+                            File.Copy(text, folder + "\\" + str2, true);
+                        }
+                        if (image.Width < image.Height)
+                        {
+                            string str2 = "Portrait_" + Path.GetFileName(text) + ".jpg";
+                            File.Copy(text, folder + "\\" + str2, true);
+                        }
+
+                        image.Dispose();
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+        }
+    }
+}
